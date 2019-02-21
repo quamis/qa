87ba758a80821f19714501cde885e1326aa2893f
Lucian Sirbu <quamis@gmail.com>
2019-02-21 22:48:54 +0200

    preprocess works as expected!!

--- v5 preprocessor/rs_v1_012/src/main.rs
+++ v5 preprocessor/rs_v1_012/src/main.rs
@@ -8,34 +8,55 @@ fn main() {
     let mut data = vec!(0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, );
     println!("{}", crc32::checksum_ieee(&data));
 
-    print!("{:?}", preprocess(&mut data));
+    let data_compressed = preprocess(&mut data);
+    print!("compressed to {} bytes: {:x?}", data_compressed.len(), data_compressed);
 }
 
 fn preprocess(data: &mut Vec<u8>)  -> Vec<u8> {
-    let mut data_compressed = Vec::new();
-    let mut bitSum = vec![0;8];
-    let mut runningSum = vec![0;8];
-    let mut bitsList = vec![Vec::new();8];
+    let mut data_compressed: Vec<u8> = vec![];
+    let mut bit_sum: Vec<u32> = vec![0;8];
+    let mut running_sum: Vec<u32> = vec![0;8];
+    let mut bit_list = vec![Vec::<u8>::new();8];
 
     for c in data.iter().enumerate() {
         println!("{:?}", c.1);
-        for bp in (0..8) {
-            let b = ((c.1 & (1 << bp)) >> bp);
-            bitSum[bp]+= u32::from(b);
+        for bp in 0..8 {
+            let b = (c.1 & (1 << bp)) >> bp;
+            bit_sum[bp]+= u32::from(b);
 
             if b!=0 {
-                runningSum[bp]+= (data.len() - c.0) as u32;
+                running_sum[bp]+= (data.len() - c.0) as u32;
             }
             
-            bitsList[bp].push(b);
+            bit_list[bp].push(b);
 
             println!("{} {}", bp, b); 
         }
     }
 
     println!("Input length: {}", data.len());
-    println!("    bit sums: {}, {}, {}, {}, {}, {}, {}, {}", bitSum[0], bitSum[1], bitSum[2], bitSum[3], bitSum[4], bitSum[5], bitSum[6], bitSum[7]);
-    println!("        sums: {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}", runningSum[0], runningSum[1], runningSum[2], runningSum[3], runningSum[4], runningSum[5], runningSum[6], runningSum[7]);
+    println!("    bit sums: {}, {}, {}, {}, {}, {}, {}, {}", bit_sum[0], bit_sum[1], bit_sum[2], bit_sum[3], bit_sum[4], bit_sum[5], bit_sum[6], bit_sum[7]);
+    println!("        sums: {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}, {:04x}", running_sum[0], running_sum[1], running_sum[2], running_sum[3], running_sum[4], running_sum[5], running_sum[6], running_sum[7]);
+
+    // data_compressed.push(data.len());
+
+    use byteorder::{BigEndian,WriteBytesExt};
+    data_compressed.write_u16::<BigEndian>(data.len() as u16).unwrap();
+    for bp in 0..8 {
+        // data_compressed.push(bit_sum[bp]);
+        data_compressed.write_u16::<BigEndian>(bit_sum[bp] as u16).unwrap();
+    }
+    for bp in 0..8 {
+        // data_compressed.push(running_sum[bp]);
+        data_compressed.write_u16::<BigEndian>(running_sum[bp] as u16).unwrap();
+    }
+
+    use crc::{crc32};
+    for bp in 0..8 {
+        // data_compressed.push(crc32::checksum_ieee(&bit_list[bp]));
+        data_compressed.write_u32::<BigEndian>(crc32::checksum_ieee(&bit_list[bp])).unwrap();
+    }
+    
 
     return data_compressed;
 }
