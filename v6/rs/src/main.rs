ffac75056a7ce5d6e27227c0345f7e8f163225b2
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2021-09-17 09:53:05 +0300

    advanced 2 more lines of code

--- v6/rs/src/main.rs
+++ v6/rs/src/main.rs
@@ -1,3 +1,26 @@
+use crc32fast::Hasher;
+use md5::{Md5, Digest};
+use std::convert::TryFrom;
+
 fn main() {
-    print!("\n\nasd\n\n");
+    let str = "abacabacabacabacabacabac";
+    print!("Initial string: {}", str);
 }
+
+
+fn myhash1(str: &[u8], _letter: u8) -> u32 {
+    let mut hasher = Hasher::new();
+    hasher.update(str);
+    return hasher.finalize();
+}
+
+fn myhash2(str: &[u8], _letter: u8) -> [u8; 16] {
+    let mut hasher = Md5::new();
+    hasher.input(str);
+    let result = hasher.result();
+    // return result[..];
+    return TryFrom::try_from(&result[0..16]).unwrap()
+}
+
+
+fn generateHash(str: &[u8], letter: u8)
