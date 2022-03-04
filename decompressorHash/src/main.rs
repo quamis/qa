964efe37bab1d15c351df90de54d2375196ab3a4
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2022-03-04 10:46:26 +0200

    cc

--- decompressorHash/src/main.rs
+++ decompressorHash/src/main.rs
@@ -34,12 +34,12 @@ fn main() {
 
     println!("fileWithCharacterTable {:#?}!", data);
 
+    // this version take up a LOP of RAM because of the .unique() call
     // for perm in data.iter().permutations(data.len()).unique() {
     // for perm in data.into_iter().permutations(data_len) {
     //     let perm_as_vec_of_chars = perm.iter().map(|b| **b as char).collect::<Vec<_>>();
     //     let perm_as_vec_of_bytes = perm.iter().map(|b| **b).collect::<Vec<_>>();
     //     let digest = md5::compute(perm_as_vec_of_bytes);
-
     //     // println!("try {:?}: {:?}", perm.iter().map(|b| **b as char).join(""), digest);
     //     if HEXLOWER.encode(digest.as_ref())==args.md5 {
     //         println!("found!!! {:?}: {:?}", perm_as_vec_of_chars, digest);
@@ -47,22 +47,12 @@ fn main() {
     //     }
     // }
 
-    let result = data.into_iter().permutations(data_len)
-        .find_first(
-            |perm| {
-                if rng.gen() {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            });
 
-    println!("found!!! {:?}", result);
-
+    // works, single CPU
+    // use as: cargo run -- --md5 6624a35d3fedaacec13f7af3e51b491f --file-with-character-table ./utest001-6624a35d3fedaacec13f7af3e51b491f-abac.txt
     // for perm in data.into_iter().permutations(data_len) {
-    //     let perm_as_vec_of_chars = perm.iter().map(|b| **b as char).collect::<Vec<_>>();
-    //     let perm_as_vec_of_bytes = perm.iter().map(|b| **b).collect::<Vec<_>>();
+    //     let perm_as_vec_of_chars = perm.iter().map(|b| *b as char).collect::<Vec<_>>();
+    //     let perm_as_vec_of_bytes = perm.iter().map(|b| *b).collect::<Vec<_>>();
     //     let digest = md5::compute(perm_as_vec_of_bytes);
 
     //     // println!("try {:?}: {:?}", perm.iter().map(|b| **b as char).join(""), digest);
@@ -71,4 +61,43 @@ fn main() {
     //         return;
     //     }
     // }
+
+
+    // rayon test, multi-CPUs
+    let data_iterator = data.into_iter().permutations(data_len);
+    let pool = rayon::ThreadPoolBuilder::new().num_threads(2).build().unwrap();
+    let n = pool.install(|| permutations_scanner(data_iterator));
+    println!("{}", n);
+
+
+    fn permutations_scanner(mut data_iterator: itertools::Permutations<std::vec::IntoIter<u8>>) -> usize {
+        let perm = data_iterator.next().unwrap();
+        let perm_as_vec_of_chars = perm.iter().map(|b| *b as char).collect::<Vec<_>>();
+        let perm_as_vec_of_bytes = perm.iter().map(|b| *b).collect::<Vec<_>>();
+        let digest = md5::compute(perm_as_vec_of_bytes);
+
+        println!("try {:?}: {:?}", perm.iter().map(|b| *b as char).join(""), digest);
+        if HEXLOWER.encode(digest.as_ref())=="6624a35d3fedaacec13f7af3e51b491f" {
+            println!("found!!! {:?}: {:?}", perm_as_vec_of_chars, digest);
+            return 1;
+        }
+        rayon::join(|| permutations_scanner(data_iterator), || permutations_scanner(data_iterator));
+
+        return 1;
+    }
+
+
+
+    // let result = data.into_iter().permutations(data_len)
+    //     .find_first(
+    //         |perm| {
+    //             if rng.gen() {
+    //                 return true;
+    //             }
+    //             else {
+    //                 return false;
+    //             }
+    //         });
+
+    // println!("found!!! {:?}", result);
 }
