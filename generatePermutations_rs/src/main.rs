85b2cfaa4cbd4ddf77193508f76c0d13e1617d17
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2022-03-17 13:40:24 +0200

    cc

--- generatePermutations_rs/src/main.rs
+++ generatePermutations_rs/src/main.rs
@@ -23,6 +23,14 @@ struct Args {
     characterTable: String,
 }
 
+/**
+ * see latest nightlies for the latest version of this at https://rust-lang.github.io/rustup-components-history/
+ * rustup toolchain install nightly-2022-02-23
+ * rustup default nightly-2022-02-23
+ *
+ *
+ *
+ */
 fn main() {
     let args = Args::parse();
 
@@ -68,6 +76,87 @@ fn itertools_permutations(data: &[u8], data_len: usize) -> u32 {
 
     return permutations_counter;
 }
+
+
+/**
+ * permutations:
+ *  reset && cargo test utest_len_3_fike_permutations -- --nocapture
+ *  reset && cargo bench utest_len_3_fike_permutations -- --nocapture
+ *
+ */
+fn fike_permutations_rec(data:&mut Vec<u8>, data_len: usize, s:usize) -> u32 {
+    let mut permutations_counter = 0;
+    let i: usize;
+    if (s==data_len) {
+        println!("try: {:?}", data.iter().map(|b| *b as char).join(""));
+        return 1;
+    }
+
+    for i in 0..(data_len-s) {
+        data.swap(s, s+i);
+        permutations_counter+= fike_permutations_rec(data, data_len, s+1);
+        data.swap(s, s+i);
+    }
+
+    return permutations_counter;
+}
+
+
+#[test]
+fn utest_len_3_fike_permutations() {
+    let data = "123".as_bytes();
+    let data_len = data.len();
+
+    let mut permutations_counter = fike_permutations_rec(&mut data.to_vec(), data_len, 0);
+
+    println!("permutations_counter: {:?}", permutations_counter);
+
+    assert_eq!(permutations_counter, 6);
+}
+
+#[test]
+fn utest_len_4_fike_permutations() {
+    let data = "1234".as_bytes();
+    let data_len = data.len();
+
+    let mut permutations_counter = fike_permutations_rec(&mut data.to_vec(), data_len, 0);
+
+    println!("permutations_counter: {:?}", permutations_counter);
+
+    assert_eq!(permutations_counter, 24);
+}
+
+/**
+ * reset && cargo bench ubench_len_4_fike_permutations
+ */
+
+#[bench]
+fn ubench_len_4_fike_permutations(b: &mut Bencher) {
+    let data = "1234".as_bytes();
+    let data_len = data.len();
+
+    println!("fileWithCharacterTable {:#?}!", data);
+
+    b.iter(|| fike_permutations_rec(&mut data.to_vec(), data_len, 0));
+}
+
+#[bench]
+fn ubench_len_5_fike_permutations(b: &mut Bencher) {
+    let data = "12345".as_bytes();
+    let data_len = data.len();
+
+    b.iter(|| fike_permutations_rec(&mut data.to_vec(), data_len, 0));
+}
+
+#[bench]
+fn ubench_len_6_fike_permutations(b: &mut Bencher) {
+    let data = "123456".as_bytes();
+    let data_len = data.len();
+
+    b.iter(|| fike_permutations_rec(&mut data.to_vec(), data_len, 0));
+}
+
+///////////////////////////////////////////////////////////////
 
 #[test]
 fn utest_len_3_itertools_permutations() {
